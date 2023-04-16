a07ed0eb7f2ccb02c532d58c75a6baf041f692a5
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2023-04-16 17:07:39 +0200

    Add the execution role statements needed to deploy

--- modules/improved_couscous/main.tf
+++ modules/improved_couscous/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 resource "aws_iam_user" "ops" {
   name = "improved-couscous-ops"
 }
@@ -39,8 +43,148 @@ data "aws_iam_policy_document" "this" {
   }
   statement {
     effect    = "Allow"
-    actions   = ["s3:PutObject", "s3:DeleteObject"]
+    actions   = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:CreateMultipartUpload",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts",
+      "s3:ListBucketMultipartUploads",
+    ]
     resources = ["${aws_s3_bucket.this.arn}/*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:GetRole", "iam:PassRole"]
+    resources = [aws_iam_role.this.arn]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "lambda:AddPermission",
+      "lambda:CreateFunction",
+      "lambda:DeleteFunction",
+      "lambda:GetFunction",
+      "lambda:GetFunctionConfiguration",
+      "lambda:GetPolicy",
+      "lambda:InvokeAsync",
+      "lambda:InvokeFunction",
+      "lambda:ListVersionsByFunction",
+      "lambda:RemovePermission",
+      "lambda:UpdateFunctionCode",
+      "lambda:UpdateFunctionConfiguration",
+    ]
+    resources = [
+      join("", [
+        "arn:aws:lambda:",
+        data.aws_region.current.name,
+        ":",
+        data.aws_caller_identity.current.account_id,
+        ":function:",
+        "improved-couscous-production",
+      ]),
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "events:ListRuleNamesByTarget",
+    ]
+    resources = [
+      join("", [
+        "arn:aws:events:",
+        data.aws_region.current.name,
+        ":",
+        data.aws_caller_identity.current.account_id,
+        ":rule/*",
+      ]),
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "events:DeleteRule",
+      "events:DescribeRule",
+      "events:ListTargetsByRule",
+      "events:PutRule",
+      "events:PutTargets",
+      "events:RemoveTargets",
+    ]
+    resources = [
+      join("", [
+        "arn:aws:events:",
+        data.aws_region.current.name,
+        ":",
+        data.aws_caller_identity.current.account_id,
+        ":rule/*zappa-keep-warm-handler*",
+      ]),
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "cloudformation:CreateStack",
+      "cloudformation:DeleteStack",
+      "cloudformation:DescribeStackResource",
+      "cloudformation:DescribeStacks",
+      "cloudformation:ListStackResources",
+      "cloudformation:UpdateStack",
+    ]
+    resources = [
+      join("", [
+        "arn:aws:cloudformation:",
+        data.aws_region.current.name,
+        ":",
+        data.aws_caller_identity.current.account_id,
+        ":stack/improved-couscous-production/*",
+      ]),
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "apigateway:POST",
+      "apigateway:GET",
+    ]
+    resources = [
+      join("", [
+        "arn:aws:apigateway:",
+        data.aws_region.current.name,
+        "::/restapis",
+      ]),
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "apigateway:DELETE",
+      "apigateway:GET",
+      "apigateway:PATCH",
+    ]
+    resources = [
+      join("", [
+        "arn:aws:apigateway:",
+        data.aws_region.current.name,
+        "::/restapis/*",
+      ]),
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "logs:DescribeLogStreams",
+      "logs:FilterLogEvents",
+    ]
+    resources = [
+      join("", [
+       "arn:aws:logs:",
+       data.aws_region.current.name,
+       ":",
+       data.aws_caller_identity.current.account_id,
+       ":log-group:*",
+      ]),
+    ]
   }
 }
 
