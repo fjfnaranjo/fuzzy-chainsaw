b079212bbaa42605ab3cb0b638912f742a4a9f9f
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2023-04-16 15:57:54 +0200

    Add Terraform definitions for the Zappa role and the S3 bucket

--- modules/improved_couscous/main.tf
+++ modules/improved_couscous/main.tf
@@ -7,27 +7,52 @@ resource "aws_iam_user" "ops" {
   name = "improved-couscous-ops"
 }
 
-resource "aws_iam_policy" "ops" {
-  name        = "improved-couscous-ops"
-  description = "improved-couscous API policy for deployment using Zappa"
+resource "aws_s3_bucket" "this" {
+  bucket   = "improved-couscous"
+}
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action = [
-          "ec2:Describe*",
-        ]
-        Resource = "*"
-      },
-    ]
-  })
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = [
+        "apigateway.amazonaws.com",
+        "lambda.amazonaws.com",
+        "events.amazonaws.com",
+      ]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "improved-couscous-lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.this.arn]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject", "s3:DeleteObject"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name        = "improved-couscous"
+  description = "improved-couscous API policy for deployment using Zappa"
+  policy      = data.aws_iam_policy_document.this.json
 }
 
 resource "aws_iam_user_policy_attachment" "ops" {
   user       = aws_iam_user.ops.name
-  policy_arn = aws_iam_policy.ops.arn
+  policy_arn = aws_iam_policy.this.arn
 }
 
 resource "aws_iam_access_key" "ops" {
